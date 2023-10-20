02210c09edf8db3e0268b6c19005afc128c76855
bsihame <bsihame@hotmail.com>
2023-10-20 08:07:55 -0400

    Get unique value using array method like set and filter methode

--- array.js
+++ array.js
@@ -1,4 +1,5 @@
 console.log("Array Methods");
+
 const arr = [
   "Marie",
   "Natalia",
@@ -22,7 +23,7 @@ console.log(obj1);
 
 const arr1 = [...new Set(arr)];
 // this will return a new array with non repetitive words
-console.log(arr1);
+console.log(arr1); // [ 'Marie', 'Natalia', 'Jamal', 'Jake', 'James', 'Nadia' ]
 
 const arr2 = [
   "Marie",
@@ -47,4 +48,12 @@ const arr2 = [
 
 const arr3 = [...new Set(arr2)];
 // Using set it does not remove space or an empty string. the empty space will be defined as undefined
-console.log(arr3);
+console.log(arr3); // ['Marie', 'Natalia','Jamal', 'Jake','James', 'Nadia','', undefined]. A set creates a unique values but still have undefined and empty string
+
+// To remove any empty string or undefined values we use filter . ==> if the value is falsy value we should remove it.
+const arr4 = arr2.filter(Boolean);
+console.log(arr4); // ['Marie', 'Natalia','Jamal', 'Jake','James', 'Nadia','Marie', 'Natalia','Jamal', 'Jake','James', 'nadia']
+
+// to remove all falsy values like empty space, undefined, and remove all repetitive words, we combine set and filter method
+const arr5 = [...new Set(arr2.filter(Boolean))];
+console.log(5, arr5); // [ 'Marie', 'Natalia', 'Jamal', 'Jake', 'James', 'Nadia' ]
